8a8556b32a98489ca5220615701f3dda006b4e6c
Israel barroso de Oliveira <israel.oliversw@gmail.com>
2026-09-17 11:15:13 -0300

    Skia funcionando

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,13 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Canvas, Rect } from "@shopify/react-native-skia";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Canvas style={{ width: 200, height: 200 }}>
+        <Rect x={0} y={0} width={200} height={200} color="#2a6fd6" />
+        <Rect x={60} y={60} width={80} height={80} color="#e4cc92" />
+      </Canvas>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
